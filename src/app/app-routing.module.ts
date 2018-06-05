0d8b48933348da712cd46a82133735310c1272a7
ruhollahm8 <graphist77@gmail.com>
2018-06-05 11:31:31 +0430

    add Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './index/index.component';
+import { QuestionComponent } from './question/question.component';
+import { QuestionsComponent } from './questions/questions.component';
 
 const routes: Routes = [
-  { path: '', component: IndexComponent }
+  { path: '', component: IndexComponent },
+  { path: 'question', component: QuestionComponent },
+  { path: 'questions', component: QuestionsComponent }
 ];
 
 @NgModule({
